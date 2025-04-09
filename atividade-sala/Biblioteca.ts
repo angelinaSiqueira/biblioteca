3ea9499273af1867272cf80e54f9dda8a9fe8f49
unknown <alexiaprangegoncalves.trabalho>
2025-04-09 08:40:42 -0300

    construtor-biblioteca

--- atividade-sala/Biblioteca.ts
+++ atividade-sala/Biblioteca.ts
@@ -2,7 +2,13 @@ import { Leitor } from "./Leitor";
 import { Livro } from "./Livro";
 
 class Biblioteca {
+    livros: Livro[] = [];
+    leitores: Leitor[] = [];
     
+    constructor(livros: Livro[] = [], leitores: Leitor[] = []){
+        this.livros = livros;
+        this.leitores = leitores
+    }
 
-    
+    const CadLeitor = new Leitor("João Paulo", 1, 991748313, 3);
 }
