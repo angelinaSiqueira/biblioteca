0484ca02e6565281f7af523a37305f664740994d
djonatanrock18@gmail.com <danielreblinn@gmail.com>
2025-04-09 08:33:04 -0300

    Biblioteca

---
+++ Djonatan-Pereira/iLeitor_iLivro.ts
@@ -0,0 +1,14 @@
+export interface iLeitor{
+  id: number;
+  nome: string;
+  telefone: string;
+  maxLivros: number;
+  livrosEmprestados: string[];
+}
+
+export interface iLivro{
+  titulo: string;
+  autor: string;
+  isbn: string;
+  status: "Disponível" | "Emprestado";
+}
